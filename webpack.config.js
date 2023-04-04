7e02340c1a3e14b596d3cb143fa798d11345c8b2
katongole isaac <katongolelsaac78@gmail.com>
2023-04-04 20:06:45 +0300

    relative publicPath fixed && favicon added

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].bundle.js",
     clean: true,
-    publicPath: "./",
+    publicPath: "/",
   },
 
   resolve: {
@@ -92,7 +92,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public/index.html"),
-      favicon: path.join(__dirname, "src/easybank/images/favicon-32x32.png"),
+      favicon: path.join(__dirname, "src/easybank/images/favicon.ico.svg"),
     }),
     new webpack.ProvidePlugin({
       React: "react",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require("webpack-merge");
 const webpackConfig = require("./webpack.config");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+// Used when hosting app on github pages.
+// *** Used to resolve the root path when hosted on github pages
+// i.e root path starting like './' instead of '/
+webpackConfig.output.publicPath = "./"
+
 module.exports = merge(webpackConfig, {
   mode: "production",
   devtool: "source-map",
